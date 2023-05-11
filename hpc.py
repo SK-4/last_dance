a174c1002ddd89155634f3fe1d6f289d7f5eab40
SK-4 <sohamk2002@gmail.com>
2023-05-11 11:37:29 +0530

    hpc

--- hpc.py
+++ hpc.py
@@ -1,7 +1,7 @@
-import streamlit as st
 import time
 import numpy as np
 import multiprocessing
+import streamlit as st
 
 def serial_multiply(mat1, mat2):
     # Multiply two matrices in serial
@@ -28,12 +28,10 @@ def parallel_multiply(mat1, mat2):
     return np.sum(results, axis=0)
 
 def main():
-    st.title("Matrix Multiplication")
-
     # Define matrix sizes
-    n = st.slider("Number of rows in matrix A", 1000, 5000, 2000)
-    m = st.slider("Number of columns in matrix A and rows in matrix B", 1000, 5000, 3000)
-    p = st.slider("Number of columns in matrix B", 1000, 5000, 4000)
+    n = st.number_input("Enter the number of rows for matrix A", value=2000)
+    m = st.number_input("Enter the number of columns for matrix A", value=3000)
+    p = st.number_input("Enter the number of columns for matrix B", value=4000)
 
     # Generate random matrices
     A = np.random.rand(n, m)
@@ -43,19 +41,19 @@ def main():
     start = time.time()
     C_serial = serial_multiply(A, B)
     end = time.time()
-    st.write("Serial multiplication took {:.4f} seconds".format(end - start))
+    st.write("Serial multiplication took {:.4f} seconds".format(end - start) + f" result is {C_serial}")
 
     # Multiply matrices in parallel
     start = time.time()
     C_parallel = parallel_multiply(A, B)
     end = time.time()
-    st.write("Parallel multiplication took {:.4f} seconds".format(end - start))
+    st.write("Parallel multiplication took {:.4f} seconds".format(end - start)+f"and result is {C_parallel}")
 
     # Check if the results match
     if np.allclose(C_serial, C_parallel):
-        st.success("The results of serial and parallel multiplication are the same")
+        st.write("The results of serial and parallel multiplication are the same")
     else:
-        st.error("The results of serial and parallel multiplication are different")
+        st.write("The results of serial and parallel multiplication are different")
 
 if __name__ == '__main__':
     main()
